84b036792699adbaefe1e24cc565c373826d9802
fitronansori <fitronansori7@gmail.com>
2024-09-19 09:38:04 +0800

    Update Logo component styling

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,8 +3,8 @@ import { logoImg } from "../assets/images";
 const Logo = () => {
   return (
     <div className="flex items-center gap-2">
-      <img src={logoImg} alt="" />
-      <p className="text-neutral text-3xl font-bold">Ajarin</p>
+      <img src={logoImg} alt="" className="size-8" />
+      <p className="text-neutral text-2xl font-bold">Ajarin</p>
     </div>
   );
 };
