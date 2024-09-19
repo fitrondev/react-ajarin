dd790f3d0c4f3a501185912936381822d580681c
fitronansori <fitronansori7@gmail.com>
2024-09-19 19:33:29 +0800

    Add login and register pages

--- src/pages/auth/Login/index.tsx
+++ src/pages/auth/Login/index.tsx
@@ -1,4 +1,63 @@
+import { Link } from "react-router-dom";
+import { loginImg } from "../../../assets/images";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section className="h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="bg-neutral flex flex-col items-center justify-center gap-4">
+        <div className="text-white">
+          <h1 className="text-4xl font-bold text-center">Login</h1>
+          <p className="text-sm text-center mt-2">
+            Welcome back, please login to your account
+          </p>
+        </div>
+
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="input your email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="input your password"
+                className="input input-bordered"
+                required
+              />
+            </div>
+
+            <div className="form-control mt-4">
+              <button className="btn btn-neutral">Login</button>
+            </div>
+
+            <div>
+              <p className="text-center mt-2">
+                Don't have an account?{" "}
+                <Link to="/auth/register" className="btn-link text-neutral">
+                  Register
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div className="hidden lg:flex items-center justify-center">
+        <img src={loginImg} alt="" loading="lazy" className="size-[600px]" />
+      </div>
+    </section>
+  );
 };
 export default Login;
--- src/pages/auth/Register/index.tsx
+++ src/pages/auth/Register/index.tsx
@@ -1,4 +1,75 @@
+import { Link } from "react-router-dom";
+import { registerImg } from "../../../assets/images";
+
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <section className="h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="bg-neutral flex flex-col items-center justify-center gap-4 order-last">
+        <div className="text-white">
+          <h1 className="text-4xl font-bold text-center">Register</h1>
+          <p className="text-sm text-center mt-2">
+            Welcome, please register to create an account
+          </p>
+        </div>
+
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="input your name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="input your email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="input your password"
+                className="input input-bordered"
+                required
+              />
+            </div>
+
+            <div className="form-control mt-4">
+              <button className="btn btn-neutral">Register</button>
+            </div>
+
+            <div>
+              <p className="text-center mt-2">
+                Already have an account?{" "}
+                <Link to="/auth/login" className="btn-link text-neutral">
+                  Login
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div className="hidden lg:flex items-center justify-center order-first">
+        <img src={registerImg} alt="" loading="lazy" className="size-[600px]" />
+      </div>
+    </section>
+  );
 };
 export default Register;
